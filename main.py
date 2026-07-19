4252ff529d1d2b16bd1f728414667e958e84459f
Diyorbek Qodirboyev <dvrk.devx@gmail.com>
2026-07-19 20:27:05 +0500

    Implement local chat functionality with translation and response handling

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from deep_translator import GoogleTranslator
 from ollama import chat
 
 # resp = chat(model="qwen2.5:0.5b", messages=[{"role": "user", "content": "Hello!"}])
@@ -6,8 +7,50 @@ from ollama import chat
 # print(resp.message.content)
 
 
-messages: list = [{"role": "user", "content": "Where is the capital of France?"}]
-stream = chat(model="qwen2.5:0.5b", messages=messages, stream=True)
-for chunk in stream:
-    print(chunk["message"]["content"], end="", flush=True)
-print("\n")
+# messages: list = [{"role": "user", "content": "Where is the capital of France?"}]
+# stream = chat(model="qwen2.5:0.5b", messages=messages, stream=True)
+# for chunk in stream:
+#     print(chunk["message"]["content"], end="", flush=True)
+# print("\n")
+
+messages: list = []
+
+print("LOCAL CHAT")
+print("Type 'exit' to quit.\n")
+
+while True:
+    user = input("You: ")
+
+    if user.lower() in {"exit", "quit"}:
+        break
+
+    english_prompt = GoogleTranslator(
+        source="auto",
+        target="en"
+    ).translate(user)
+
+    print("English prompt:", english_prompt)
+
+    messages.append({"role": "user", "content": english_prompt})
+
+    print("\nAI: ", end="", flush=True)
+
+    english_response = ""
+
+    # stream = chat(model="qwen2.5:0.5b", messages=messages, stream=True)
+
+    # for chunk in stream:
+    #     english_response = chunk.message.content
+
+    response = chat(model='qwen2.5:0.5b', messages=messages)
+    english_response = response.message.content
+
+    uzbek_response = GoogleTranslator(source="en", target="uz").translate(
+        english_response
+    )
+
+    print(uzbek_response)
+
+    messages.append({"role": "assistant", "content": english_response})
+
+    print()
